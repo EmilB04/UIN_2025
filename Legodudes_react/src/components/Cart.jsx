b0c2743dcc939758b3731f7685b221d93459f1c2
Emil Berglund <emil.berglund@live.no>
2025-02-10 14:02:03 +0100

    Working navigation - routing

--- Legodudes_react/src/components/Cart.jsx
+++ Legodudes_react/src/components/Cart.jsx
@@ -2,6 +2,11 @@
 import CartProduct from "./CartProduct";
 
 export default function Cart({ toggle, cart }) {
+  // Regn ut cart-total
+  let total = 0;
+  cart.forEach((product) => {
+    total += (product.price * product.quantity);
+  });
   return (
     <section id="cart" className={toggle ? "visible" : null}>
       <h3>Din handlevogn</h3>
@@ -11,7 +16,7 @@ export default function Cart({ toggle, cart }) {
         ))}
       </div>
       <p>
-        Totalt: Kr. <span id="cart-total">99999</span>,-
+        Totalt: Kr. <span id="cart-total">{total}</span>,-
       </p>
     </section>
   );
--- Legodudes_react/src/components/Home.jsx
+++ Legodudes_react/src/components/Home.jsx
@@ -1,27 +1,13 @@
 /* eslint-disable react/prop-types */
-import { Link } from "react-router";
-import Cart from "./Cart";
-import Header from "./Header";
 import PageContent from "./PageContent";
 
-export default function Home({toggle, cart, cartCount, setToggle, products, setCartCount, setCart}) {
+export default function Home({cart, products, setCartCount, setCart}) {
     return (
-        <div id="content">
-        {/* Legger til handlevogn-komponenten */}
-        <Cart toggle={toggle} cart={cart} />
-        {/* Overskrift-seksjon med logo og handlekurvknapp */}
-        <Header cartCount={cartCount} setToggle={setToggle} toggle={toggle} />
         <PageContent
             products={products}
             setCartCount={setCartCount}
             cart={cart}
             setCart={setCart}
         />
-
-        {/* Footer-seksjon */}
-        <footer>
-            <p>2025 &copy; Legodudes / <Link to="/about">Om nettbutikken</Link></p>
-        </footer>
-        </div>
     )
 }
