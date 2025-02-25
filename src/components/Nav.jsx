dfdf7fc414b9d213c8006203c405002e59691a43
Emil Berglund <emil.berglund@live.no>
2025-02-25 01:16:35 +0100

    Added links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,15 @@
+import { Link } from "react-router";
+
 export default function Nav() {
     return (
         <nav id="categories">
             <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>React</li>
-                <li>Sanity and headless CMS</li>
+                <Link to={'/html'}>HTML</Link>
+                <Link to={'/css'}>CSS</Link>
+                <Link to={'/javascript'}>JavaScript</Link>
+                <Link to={'/react'}>React</Link>
+                <Link to={'/sanity'}>Sanity and headless CMS</Link>
             </ul>
-
         </nav>
     )
 }
