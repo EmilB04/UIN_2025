ee0ba7a2aa05948d3f0bf6e105a032c6ef69b426
Emil <emil.berglund@live.no>
2025-03-20 09:06:12 +0100

    Forelesning: added slug URL with ruleset

---
+++ Legodudes_react_sanity/uin/schemaTypes/category.js
@@ -0,0 +1,21 @@
+export const category = {
+    type: "document",
+    name: "category",
+    title: "Kategori",
+    fields: [
+        {
+            name: "categoryname",
+            title: "Kategorinavn",
+            type: "string"
+        },
+        {
+            name: "categoryslug",
+            title: "Pen URL",
+            type: "slug",
+            options: {
+                source: "categoryname",
+                slugify: input => input.toLowerCase().replace("&", "and").replace(/\s+/g, "-").replace(/[^a-z0-9- ]/g, "")
+            }
+        }
+    ]
+}
