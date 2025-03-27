85593e48298ae10e32a0ce4c3d59d39dd71059da
Emil Berglund <emil.berglund@live.no>
2025-03-27 11:23:46 +0100

    Add sanity

---
+++ uin25ak3gruppe2/sanity.config.js
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'uin25ak3_gruppe2',
+
+  projectId: 't7exo8z2',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
