a5232292eaaf2f98261ca23425ecd4ea48bbefae
EmilB04 <emil.berglund@live.no>
2025-03-27 13:10:07 +0100

    Add Members

---
+++ gruppehjemmeside/src/scripts/GroupMembers.js
@@ -0,0 +1,20 @@
+const groupMembers = [
+    {
+        "name": "Andreas B. Olaussen",
+        "email": "andreas...@hiof.no"
+    },
+    {
+        "name": "Emil Berglund",
+        "email": "emilbe@hiof.no"
+    },
+    {
+        "name": "Sebastian W. Thomsen",
+        "email": "sebastian...@hiof.no"
+    },
+    {
+        "name": "Ida K. Tollaksen",
+        "email": "ida...@hiof.no"
+    }
+];
+
+export default groupMembers;
