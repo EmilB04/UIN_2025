a249bf8da5ddb51b036a49aed40730fa3e4444df
Emil Berglund <emil.berglund@live.no>
2025-03-31 13:13:02 +0200

    Update emails

--- gruppehjemmeside/src/scripts/GroupMembers.js
+++ gruppehjemmeside/src/scripts/GroupMembers.js
@@ -1,24 +1,24 @@
 const groupMembers = [
-    {
-        "name": "Andreas B. Olaussen",
-        "email": "andreas...@hiof.no",
-        "slug": "andreas"
-    },
-    {
-        "name": "Emil Berglund",
-        "email": "emilbe@hiof.no",
-        "slug": "emil"
-    },
-    {
-        "name": "Sebastian W. Thomsen",
-        "email": "sebastian...@hiof.no",
-        "slug": "sebastian"
-    },
-    {
-        "name": "Ida K. Tollaksen",
-        "email": "ida...@hiof.no",
-        "slug": "ida"
-    }
+  {
+    name: "Andreas B. Olaussen",
+    email: "andreabo@hiof.no",
+    slug: "andreas",
+  },
+  {
+    name: "Emil Berglund",
+    email: "emilbe@hiof.no",
+    slug: "emil",
+  },
+  {
+    name: "Sebastian W. Thomsen",
+    email: "sebastwt@hiof.no",
+    slug: "sebastian",
+  },
+  {
+    name: "Ida K. Tollaksen",
+    email: "idakto@hiof.no",
+    slug: "ida",
+  },
 ];
 
 export default groupMembers;
