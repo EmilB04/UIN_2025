5e17e7a870456c2e697638a352238e5647ff01f7
EmilB04 <emil.berglund@live.no>
2025-03-31 23:51:42 +0200

    Handeled invalid URLs. Needs further work

--- gruppehjemmeside/src/App.jsx
+++ gruppehjemmeside/src/App.jsx
@@ -1,13 +1,19 @@
 import { Route, Routes } from "react-router";
 import HomePage from "./components/HomePage";
 import MemberPage from "./components/MemberPage";
+import groupMembers from "./scripts/GroupMembers";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path=":slug" element={<MemberPage />} />
-      {/* TODO: Add fix for wrong URL's. Show 404 error*/}
+      <Route
+        path=":slug"
+        element={<MemberPage groupMembers={groupMembers} />}
+      />
+      <Route path="/404" element={<PageNotFound />} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 }
