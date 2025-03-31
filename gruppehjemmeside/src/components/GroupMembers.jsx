9c8b31b19dfdf49264bf6368550e9f040dd14188
Emil Berglund <emil.berglund@live.no>
2025-03-31 12:33:53 +0200

    Change a -> Link

--- gruppehjemmeside/src/components/GroupMembers.jsx
+++ gruppehjemmeside/src/components/GroupMembers.jsx
@@ -1,16 +1,19 @@
+import { Link } from "react-router";
 import groupMembers from "../scripts/GroupMembers";
 import "../styles/GroupMemberStyle.scss";
 export default function GroupMembers() {
+    
+    const groupMember = "/:slug"; // Define the base URL for group members
     return (
         <section>
             <h1>Gruppemedlemmer</h1>
             <div>
                 {groupMembers.map((member, index) => (
-                    <a href="" key={index}>
+                    <Link to={groupMember} key={index}>
                         <img src="https://placehold.co/400x400" alt="" />
                         <h2>{member.name}</h2> {/* Navn */}
                         <h3>{member.email}</h3> {/* Epost */}
-                    </a>
+                    </Link>
                 ))}
             </div>
         </section>
