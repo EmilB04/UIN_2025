c1580de2b291b751173ed08eae52cff0782aef13
Emil Berglund <emil.berglund@live.no>
2025-03-31 13:31:32 +0200

    Made links dynamic

--- gruppehjemmeside/src/components/GroupMembers.jsx
+++ gruppehjemmeside/src/components/GroupMembers.jsx
@@ -1,15 +1,14 @@
 import { Link } from "react-router";
 import groupMembers from "../scripts/GroupMembers";
 import "../styles/GroupMemberStyle.scss";
+
 export default function GroupMembers() {
-    
-    const groupMember = "/:slug"; // Define the base URL for group members
     return (
         <section>
             <h1>Gruppemedlemmer</h1>
             <div>
                 {groupMembers.map((member, index) => (
-                    <Link to={groupMember} key={index}>
+                    <Link to={`/${member.slug}`} key={index}>
                         <img src="https://placehold.co/400x400" alt="" />
                         <h2>{member.name}</h2> {/* Navn */}
                         <h3>{member.email}</h3> {/* Epost */}
