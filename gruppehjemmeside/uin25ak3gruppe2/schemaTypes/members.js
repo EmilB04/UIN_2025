2082fc7136901942ebd57658631331bc11e9cb90
Emil Berglund <emil.berglund@live.no>
2025-03-31 13:47:27 +0200

    Create sanity document for members

---
+++ gruppehjemmeside/uin25ak3gruppe2/schemaTypes/members.js
@@ -0,0 +1,50 @@
+export const schemaTypes = [
+    {
+        name: 'members',
+        title: 'Group Members',
+        type: 'document',
+        fields: [
+            {
+                name: 'name',
+                title: 'Name',
+                type: 'string',
+            },
+            {
+                name: 'email',
+                title: 'Email',
+                type: 'string',
+            },
+            {
+                name: 'slug',
+                title: 'Slug',
+                type: 'slug',
+                options: {
+                    source: (doc) => doc.name.split(' ')[0].toLowerCase(),
+                    maxLength: 96,
+                },
+            },
+            {
+                name: 'photo',
+                title: 'Photo',
+                type: 'image',
+                options: {
+                    hotspot: true,
+                },
+            },
+            {
+                name: 'bio',
+                title: 'Bio',
+                type: 'text',
+                options: {
+                    maxLength: 200,
+                },
+            },
+            {
+                name: 'interests',
+                title: 'Interests',
+                type: 'array',
+                of: [{ type: 'string' }],
+            },
+        ],
+    },
+];
