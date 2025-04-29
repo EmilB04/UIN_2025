aade224506435e20e93b96d4b2a12723f4d362cc
Ida Tollaksen <ida.k.tollaksen@gmail.com>
2025-04-29 22:14:40 +0200

    refactor: cleaned up HomePage by moving Sanity fetching to eventServices

--- Billettlyst/src/pages/HomePage.jsx
+++ Billettlyst/src/pages/HomePage.jsx
@@ -1,12 +1,10 @@
 import { useEffect, useState } from "react";
-import { client } from "../sanity/client";
 import EventCard from "../components/EventCard";
-import "../styles/homePageStyle.scss"
+import "../styles/homePageStyle.scss";
 
 const API_KEY = "JhVG7XhAqV9xT9vUUMWiJFzFjA5lVWG9";
 
 export default function HomePage() {
-  const [events, setEvents] = useState([]);
   const [selectedCity, setSelectedCity] = useState("Oslo");
   const [apiEvents, setApiEvents] = useState([]);
 
@@ -19,31 +17,10 @@ export default function HomePage() {
       const events = data._embedded?.events || [];
       setApiEvents(events);
     } catch (error) {
-      console.error("Error fetching events:", error);
+      console.error("Error fetching city events:", error);
     }
   };
 
-  useEffect(() => {
-    const fetchEvents = async () => {
-      const query = `*[_type == "event"]{
-        _id,
-        apiId,
-        title,
-        date,
-        time,
-        country,
-        city,
-        venue,
-        "image": image.asset->url
-      }`;
-      const data = await client.fetch(query);
-      setEvents(data);
-    };
-
-    fetchEvents();
-  }, []);
-
-  // Fetch events for the selected city when it changes
   useEffect(() => {
     if (selectedCity) {
       fetchCityEvents(selectedCity);
@@ -54,8 +31,8 @@ export default function HomePage() {
     <div id="HomePage">
       <section id="Festivaler">
         <h2>Sommerens festivaler!</h2>
-        {/**Her kommer festivaler */}
       </section>
+
       <section id="Storbyer">
         <h2>Hva skjer i verdens storbyer!</h2>
         <span>
@@ -67,11 +44,10 @@ export default function HomePage() {
         </span>
 
         <h2>Hva skjer i {selectedCity}</h2>
-
         <div id="EventsHomePage">
           {apiEvents.length > 0 ? (
             apiEvents.map((event) => (
-              <EventCard 
+              <EventCard
                 key={event.id}
                 image={event.images?.[0]?.url}
                 name={event.name}
