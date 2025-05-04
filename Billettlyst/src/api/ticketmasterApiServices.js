9b2551a939f459a29aa73ab09966fe4332369ed3
Thomsen97 <sebastwt@hiof.no>
2025-05-04 04:01:48 +0200

    Added events based on category on the different category pages
    - Updated ticketmasterApiServices.js with event fetches for categories: Musikk, Sport, Teater/Show.
    
    - Updated CategoryPage.jsx to now show events relevant to the current category.
    
    - Made categoryPageStyle.scss for styling CategoryPage

--- Billettlyst/src/api/ticketmasterApiServices.js
+++ Billettlyst/src/api/ticketmasterApiServices.js
@@ -4,8 +4,87 @@ const URL =  import.meta.env.VITE_TICKETMASTER_BASE_URL;
 export const getSpecificFestival = async (festivalName, setFestival) => {
     fetch(`${URL}/events.json?apikey=${API_KEY}&keyword=${festivalName}&countryCode=NO&locale=no-no`)
         .then((response) => response.json())
-        .then((data) => setFestival(data._embedded?.events?.[0]))
+        .then((data) => {
+            console.log(`Festival data for ${festivalName}:`, data);
+            //console.log(`Alle treff for ${festivalName}:`, data._embedded?.events);
+            setFestival(data._embedded?.events?.[0]);
+        })
         .catch((error) => 
             console.error("Error fetching festival data:", error)
         );
 };
+
+export const fetchCityEvents = async (city) => {
+    try {
+      const response = await fetch(
+        `${URL}/events.json?city=${city}&size=10&apikey=${API_KEY}`
+      );
+      const data = await response.json();
+      return data._embedded?.events || [];
+    } catch (error) {
+      console.error("Error fetching events:", error);
+    }
+  };
+
+export const getEventById = async (id) => {
+  try{
+    const response = await fetch(`${URL}/events/${id}.json?apikey=${API_KEY}`);
+    const data = await response.json();
+    return data;
+  } catch (error){
+    console.error("Error fetching event by ID:", error);
+    return null;
+  }
+};
+
+export const getFestivalPassesByKeyword = async (keyword) => {
+  try{
+    const response = await fetch(
+      `${URL}/events.json?apikey=${API_KEY}&keyword=${keyword}&countryCode=NO&locale=no-no`
+    );
+    const data = await response.json();
+    return data._embedded?.events || [];
+  } catch (error) {
+    console.error("Error fetching festival passes:", error);
+    return [];
+  }
+}
+
+  // Funksjon for å hente alle musikk-events
+  export const fetchMusicEvents = async () => {
+    try {
+      const response = await fetch(
+        `${URL}/events.json?apikey=${API_KEY}&classificationName=Music&countryCode=NO&size=8`
+      );
+      const data = await response.json();
+      return data._embedded?.events || [];
+    } catch (error) {
+      console.error("Error fetching music events:", error);
+    }
+  };
+  
+  // Funksjon for å hente alle sport-events
+  export const fetchSportsEvents = async () => {
+    try {
+      const response = await fetch(
+        `${URL}/events.json?apikey=${API_KEY}&classificationName=Sports&countryCode=NO&size=8`
+      );
+      const data = await response.json();
+      return data._embedded?.events || [];
+    } catch (error) {
+      console.error("Error fetching music events:", error);
+    }
+  };
+  
+  // Funksjon for å hente teater/show-events
+  export const fetchTheatreEvents = async () => {
+    try {
+      const response = await fetch(
+        `${URL}/events.json?apikey=${API_KEY}&segmentId=KZFzniwnSyZfZ7v7na&countryCode=NO&size=8`
+      );
+      const data = await response.json();
+      return data._embedded?.events || [];
+    } catch (error) {
+      console.error("Error fetching theatre events:", error);
+    }
+  };
